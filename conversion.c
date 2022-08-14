0be2361f97c7b3651fad58cc9cd1d06ac9107ff3
David Petzold <C25David.Petzold@afacademy.af.edu>
2022-08-14 00:39:18 -0600

    Why don't they tell us what format stuff is?

--- conversion.c
+++ conversion.c
@@ -21,18 +21,18 @@ int main() {
     float floatQuotient3 = intVar1 / (float)intVar2;
 
     // Task 2 -----------------------------------------------------------------------
-    printf("%i\n", intQuotient);
-    printf("%.10f\n", floatQuotient1);
-    printf("%.10f\n", floatQuotient2);
-    printf("%.10f\n", floatQuotient3);
+    printf("intQuotient = intVar1 / intVar2 = %i\n", intQuotient);
+    printf("floatQuotient1 = intVar1 / intVar2 = %.10f\n", floatQuotient1);
+    printf("floatQuotient2 = intVar1 / intVar2 = %.10f\n", floatQuotient2);
+    printf("floatQuotient3 = intVar1 / intVar2 = %.10f\n", floatQuotient3);
 
 
     // Task 3 -----------------------------------------------------------------------
     double doubleQuotient1 = (float)intVar1/intVar2;
     double doubleQuotient2 = (double)intVar1/intVar2;
 
-    printf("%.10f\n", doubleQuotient1);
-    printf("%.10f\n", doubleQuotient2);
+    printf("doubleQuotient1 = intVar1 / intVar2 = %.10f\n", doubleQuotient1);
+    printf("doubleQuotient2 = intVar1 / intVar2 = %.10f\n", doubleQuotient2);
 
     return 0;
 }
